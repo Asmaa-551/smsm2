0876c2fbd9a5a08901d451a40f994de56e1790bc
noor <noorazzam721@gmail.com>
2024-10-24 23:27:21 +0400

    clean

--- testBST.java
+++ testBST.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class testBST {
-    
+
     private BST environmentalTree;
     private Scanner scanner;
 
     // Constructor
     public testBST() {
-        environmentalTree = new BST();
+        environmentalTree = new BST(); // Assuming BST class is implemented elsewhere
         scanner = new Scanner(System.in);
     }
 
@@ -16,32 +16,56 @@ public class testBST {
         System.out.println("------------------------------------------------------------");
         System.out.println("Environmental Quality Monitoring System");
         System.out.println("------------------------------------------------------------");
-        System.out.println("1. Log new environmental data");
+        System.out.println("1. Insert new environmental data");
         System.out.println("2. Update existing environmental data");
         System.out.println("3. Delete environmental data");
-        System.out.println("4. View environmental report");
+        System.out.println("4. Search for specific environmental data");
+        System.out.println("5. Display environmental rankings (Best to Worst)");
+        System.out.println("6. Display environmental rankings (Worst to Best)");
+        System.out.println("7. Backup data");
+        System.out.println("8. Restore data from backup");
         System.out.println("0. Exit");
         System.out.println("------------------------------------------------------------");
     }
 
-    // Method for logging new environmental data
-    public void logNewData() {
-        // Implementation to be added
+    // Method for inserting new environmental data
+    public void insertNewData() {
+        // Empty method (implementation to be added later)
     }
 
     // Method for updating existing environmental data
     public void updateData() {
-        // Implementation to be added
+        // Empty method (implementation to be added later)
     }
 
     // Method for deleting environmental data
     public void deleteData() {
-        // Implementation to be added
+        // Empty method (implementation to be added later)
     }
 
-    // Method for viewing the environmental report
-    public void viewReport() {
-        // Implementation to be added
+    // Method for searching specific environmental data
+    public void searchData() {
+        // Empty method (implementation to be added later)
+    }
+
+    // Method for displaying environmental rankings (Best to Worst)
+    public void displayBestToWorst() {
+        // Empty method (implementation to be added later)
+    }
+
+    // Method for displaying environmental rankings (Worst to Best)
+    public void displayWorstToBest() {
+        // Empty method (implementation to be added later)
+    }
+
+    // Method for backing up data
+    public void backupData() {
+        // Empty method (implementation to be added later)
+    }
+
+    // Method for restoring data from backup
+    public void restoreData() {
+        // Empty method (implementation to be added later)
     }
 
     // Method to handle user input and menu navigation
@@ -53,7 +77,7 @@ public class testBST {
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    logNewData();
+                    insertNewData();
                     break;
                 case 2:
                     updateData();
@@ -62,7 +86,19 @@ public class testBST {
                     deleteData();
                     break;
                 case 4:
-                    viewReport();
+                    searchData();
+                    break;
+                case 5:
+                    displayBestToWorst();
+                    break;
+                case 6:
+                    displayWorstToBest();
+                    break;
+                case 7:
+                    backupData();
+                    break;
+                case 8:
+                    restoreData();
                     break;
                 case 0:
                     System.out.println("Exiting...");
